c857b7af339e7a9d3dc1f17839857fba0066fe39
juanda151005 <juandavidvr151005@gmail.com>
2026-03-12 19:46:54 -0500

    Ajuste de limites horizontales

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,23 @@ public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed = 2;
     public float horizontalSpeed = 3;
+    public float rightLimit = 5.5f;
+    public float leftLimit = -5.5f;
     
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
         if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
         {
-            transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+            if (this.gameObject.transform.position.x > leftLimit){
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+            }
         }
         if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
         {
-            transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
+            if (this.gameObject.transform.position.x < rightLimit){
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
+            }
         }
     }
 }
